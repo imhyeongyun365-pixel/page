87e4c1d93352989cbb52ac14205adaf499324e12
unknown <imhyeongyun365@gmail.com>
2026-01-29 12:33:22 +0900

    자바스크립트,배경추가

--- js/app.js
+++ js/app.js
@@ -12,7 +12,9 @@ navList.forEach((btn,i)=>{
         console.log(document.querySelector(`$sec-${i+1}`))
 
 
-        document.querySelector(`#sec-${i+1}`).scrollIntoView 
+        document.querySelector(`#sec-${i+1}`).scrollIntoView({
+            behavior:'smooth'
+        }) 
 
     })
 })
@@ -40,5 +42,32 @@ fixedArrow.addEventListener('click', ()=>{
     window.scrollTo({
         top:0,
         behavior:'smooth'
+    })
+})
+
+const sec5MenuList = document.querySelectorAll(".sec-5-menu li")
+const sec5ContentList = document.querySelectorAll(".sec-5-content-wrap .content")
+let crr = 0;
+
+init(crr);
+
+function init(x) {
+    sec5MenuList[x].classList.add("active")
+    sec5ContentList[x].classList.add("on")
+}
+
+function reset() {
+    for (let i = 0; i < sec5MenuList.length; i++) {
+        sec5MenuList[i].classList.remove("active")
+        sec5ContentList[i].classList.remove("on")
+    }
+}
+
+sec5MenuList.forEach((menu, index) =>{
+    menu.addEventListener("click", (e)=>{
+        e.preventDefault()
+
+        reset()
+        init(index)
     })
 })
